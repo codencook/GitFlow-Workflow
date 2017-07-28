51a490a9d32ea1a6c6522801b13fab3a3128352c
codencook <arajman1@binghamton.edu>
2017-07-27 22:59:38 -0500

    Commit - Transactions pckg added to develop

--- src/com/driver/Driver.java
+++ src/com/driver/Driver.java
@@ -1,6 +1,9 @@
 package com.driver;
 
 import com.Customer.Customer;
+import com.Transaction.Transaction;
+
+import java.util.Date;
 
 /**
  * Created by Aniket on 7/27/2017.
@@ -8,8 +11,12 @@ import com.Customer.Customer;
 public class Driver {
     public static void main(String[] args) {
         System.out.println(" *** GitFlow Workflow Branching *** ");
-        Customer c1=new Customer(111,"Jack",500.0);
-        System.out.println(c1);
+        Customer custObj1=new Customer(111,"Jack",500.0);
+        System.out.println(custObj1);
+        Date todaysDate = new Date();
+        Transaction transObj1=new Transaction(123456,1200,todaysDate);
+        System.out.println(transObj1);
+
 
     }
 }
