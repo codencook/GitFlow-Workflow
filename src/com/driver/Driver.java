00a79fd82d457573b6b9f3a6e14fa5107cd29620
codencook <arajman1@binghamton.edu>
2017-07-27 22:37:11 -0500

    Commit - Cust pckg added to develop

--- src/com/driver/Driver.java
+++ src/com/driver/Driver.java
@@ -1,10 +1,15 @@
 package com.driver;
 
+import com.Customer.Customer;
+
 /**
  * Created by Aniket on 7/27/2017.
  */
 public class Driver {
     public static void main(String[] args) {
         System.out.println(" *** GitFlow Workflow Branching *** ");
+        Customer c1=new Customer(111,"Jack",500.0);
+        System.out.println(c1);
+
     }
 }
